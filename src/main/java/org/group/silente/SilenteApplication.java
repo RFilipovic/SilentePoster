622a4196549d248e875f2126ebe47dbc1dbbea60
Rene <rene.filipovic03@gmail.com>
2025-02-06 19:06:34 +0100

    initial commit

---
+++ src/main/java/org/group/silente/SilenteApplication.java
@@ -0,0 +1,13 @@
+package org.group.silente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilenteApplication.class, args);
+    }
+
+}
